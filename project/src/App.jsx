803766daea3f85ef9a76a94700d794c4fadeda98
Özgür Tokay <ozytky@gmail.com>
2024-06-22 20:23:14 +0300

    chore: Update project dependencies and add shared components

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Footer } from "@/shared/components/Footer";
+import { Navbar } from "@/shared/components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container-fluid">
+        <div className="d-flex flex-column min-vh-100">
+          <Navbar />
+          <Outlet />
+        </div>
+        <Footer />
+        <ToastContainer position="top-center" theme="light" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ project/src/pages/Introduction/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Introduction = () => {
+  return (
+    <div>Introduction</div>
+  )
+}
